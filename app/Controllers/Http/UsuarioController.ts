c86454688fc4773ed19f8f3cd16c3dd5097fbe17
alexandre.nascimento.ifms <alexandre.nascimento.ifms@gmail.com>
2022-03-25 20:15:39 -0400

    rwarw

--- app/Controllers/Http/UsuarioController.ts
+++
@@ -1,7 +0,0 @@
-import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
-
-export default class UsuarioController {
-  public async index({ request }: HttpContextContract) {
-    return { teste: "testando", params: request.all() };
-  }
-}
