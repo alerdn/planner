be6c3448834edfa4e7865b10a1880e07357853f4
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2022-06-23 22:56:56 -0400

    atualização busca membros

--- start/routes/project.ts
+++ start/routes/project.ts
@@ -3,12 +3,17 @@ import Route from "@ioc:Adonis/Core/Route";
 export default (route: typeof Route) => {
 	route
 		.group(() => {
-            route.get("/", "ProjectsController.projects");   
-			route.post("/", "ProjectsController.addProject");            
-			route.get("/members", "ProjectsController.members");
+			route.get("/", "ProjectsController.projects");
+			route.post("/", "ProjectsController.addProject");
 			route.post("/members", "ProjectsController.addMember");
 			route.delete("/members", "ProjectsController.removeMember");
 		})
 		.prefix("projects")
 		.middleware("auth");
+
+	route
+		.group(() => {
+			route.get("/members", "ProjectsController.members");
+		})
+		.prefix("projects");
 };
